a887fe06df8c01d4d7bde8cc803fe5cf0c8587f6
feritydn <feritydn@gmail.com>
2020-06-20 15:45:22 -0300

    ucuncu commit from local repo

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,7 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("First commit from local repository");
-		System.out.println("First commit from local repository");
+		System.out.println("Second commit from local repository");
+		System.out.println("Third commit from local repository");
 
 	}
 
